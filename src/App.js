5a3ed8db750c6a7e48f05e2d6f14600e050f6257
Arman Moztarzadeh <armanmoztar@outlook.com>
2024-11-27 14:01:01 -0800

    feat: Include speedInsights

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Portfolio from "./Portfolio";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <div className="App">
       <Portfolio />
+      <SpeedInsights />
     </div>
   );
 }
